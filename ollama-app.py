9ea3940bf765472ebdc188d087cebb769fc48e68
Debdoot Sen <46738040+debdoot9804@users.noreply.github.com>
2025-02-28 20:51:35 +0530

    Uploaded app ,requirements and env file

---
+++ ollama-app.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+## Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]="QnA chatbot using OLLAMA- LLAMA 3.2 model "
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the questions asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo With LLama 3.2 Model")
+input_text=st.text_input("What question you have in mind?")
+
+
+## Ollama Llama2 model
+llm=Ollama(model="llama3.2")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
+
